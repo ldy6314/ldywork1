608c046409a26c7b20f3d1adeda488f254af6760
ldy6314 <497661660@qq.com>
2021-12-24 23:27:24 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/blueprint/admin.py
    #	readexecl.py

--- app/appblueprint/admin.py
+++ app/appblueprint/admin.py
@@ -1,15 +1,17 @@
 from flask import Blueprint, send_file, session, jsonify, flash, current_app, redirect, url_for
 from forms import AddForm, AddSubjectForm, UploadClassForm, AddStudentForm, UploadSubjectsForm, EditSubjectForm
 from flask import render_template
-from models import Subject
+from models import Subject, Student, Class
 from extensions import db
-from openpyxl import Workbook, load_workbook
+from openpyxl import Workbook
 from openpyxl.styles import Alignment, Border, Side
 from io import BytesIO
 from flask_login import login_required
-from utils import redirect_back, random_filename, get_subjects, get_time_list
+from utils import redirect_back, random_filename, get_subjects, get_time_list, parser_class_id,\
+    get_students_information, to_class_id
 import os
 from openpyxl.worksheet.datavalidation import DataValidation
+from urllib.parse import quote
 
 admin_bp = Blueprint('admin', __name__)
 
@@ -112,8 +114,32 @@ def class_admin():
     form = UploadClassForm()
     form1 = AddStudentForm()
     permission = session['permission']
+    class_id = session['class_id']
+    cls = Class.query.filter_by(id=class_id).first()
+    infos = []
+    class_tot = 0
+    subjects_cnt = 0
+    students_cnt = len(cls.students)
+    for student in cls.students:
+        info = [student.name, student.contact1, student.contact2]
+        tot = 0
+        subjects = student.subjects
+        for i in range(4):
+            try:
+                info.append("{}({})".format(subjects[i].name, subjects[i].price))
+                subjects_cnt += 1
+                class_tot += subjects[i].price
+                tot += subjects[i].price
+            except IndexError:
+                info.append("")
+        info.append(tot)
+        infos.append(info)
+
+    for info in infos:
+        print(info)
+    print("人数=", students_cnt, "人次=", subjects_cnt, "总费用=", class_tot)
     if permission == 1:
-        return render_template('classadmin.html', form=form, form1=form1)
+        return render_template('classadmin.html', form=form, form1=form1, infos=infos)
     return render_template("permission_deny.html")
 
 
@@ -174,7 +200,7 @@ def upload_subjects():
         for i in subjects:
             res = Subject.query.filter_by(name=i[0]).first()
             if not res:
-                subject = Subject(name=i[0], time=i[1], price=int(i[2].strip('元')), canceled=0, remark="")
+                subject = Subject(name=i[0], time=i[1], price=i[2], canceled=0, remark="")
                 infos.append(subject)
             else:
                 flash('{}已经存在！'.format(i[0]))
@@ -193,6 +219,9 @@ def upload_subjects():
 
 @admin_bp.route('/download_class_table')
 def download_class_table():
+    class_id = session['class_id']
+    grd, cls = parser_class_id(class_id)
+    print(grd, cls)
     time_list = get_time_list()
     subject_list = []
     for time in time_list:
@@ -206,11 +235,27 @@ def download_class_table():
     tm_lst = [tm+'项目' for tm in time_list]
     col_name.extend(tm_lst)
     ws.append(col_name)
+    dv = DataValidation(type="list", formula1='"一,二,三,四,五,六"', allow_blank=True)
+    dv.error = "输入的值不在下拉列表中"
+    dv.errorTitle = "错误"
+    dv.prompt = "从下拉列表中选择年级"
+    dv.promptTitle = "列表选择"
+    dv.add('B1')
+    ws.add_data_validation(dv)
+    dv1 = DataValidation(type="list", formula1='"1,2,3,4,5,6,7,8"', allow_blank=True)
+    dv1.error = "输入的值不在下拉列表中"
+    dv1.errorTitle = "错误"
+    dv1.prompt = "从下拉列表中选择班级"
+    dv1.promptTitle = "列表选择"
+    dv1.add('D1')
+    ws.add_data_validation(dv1)
+    ws['B1'].value = grd
+    ws['D1'].value = cls
     for infos in zip(subject_list, ["D3:D50", "E3:E50", "F3:F50", "G3:G50"]):
         dv = DataValidation(type="list", formula1='"{}"'.format(infos[0]), allow_blank=True)
         dv.error = "输入的值不在下拉列表中"
         dv.errorTitle = "错误"
-        dv.prompt = "请从下拉列表中选择一个值"
+        dv.prompt = "请从下拉列表中选择课程"
         dv.promptTitle = "列表选择"
         dv.add(infos[1])
         ws.add_data_validation(dv)
@@ -234,7 +279,49 @@ def download_class_table():
     wb.save(virtual_book)
     virtual_book.seek(0)
     rv = send_file(virtual_book, as_attachment=True, attachment_filename="class_table.xlsx")
-    rv.headers['Content-Disposition'] += ";filename*=utf-8' 'test.xlsx"
+    rv.headers['Content-Disposition'] += ";filename*=utf-8' '{}{}{}.xlsx".format(quote(grd), cls, quote("班级俱乐部报名表"))
     return rv
-    return "heheh"
+
+
+@admin_bp.route("/upload_class_table", methods=['GET', 'POST'])
+def upload_class_table():
+    path = os.path.join(current_app.root_path, 'upload\\')
+    print("path=", path)
+    form = UploadSubjectsForm()
+    if form.validate_on_submit():
+        f = form.file.data
+        filename = f.filename
+        f.save(path + filename)
+        try:
+            grd, cls, infos = get_students_information(path+filename)
+            class_id = to_class_id(grd+str(cls))
+            if class_id != session['class_id']:
+                flash('班级填写不正确,请修改')
+                return redirect_back('/')
+
+            for i in infos:
+                res = Student.query.filter_by(class_id=class_id, name=i[0]).all()
+                if not res:
+                    student = Student(class_id=class_id, name=i[0], contact1=i[1] if i[1] else "", contact2=i[2] if i[2]
+                                      else "")
+
+                    for sub in i[-4:]:
+                        if sub:
+                            sub = Subject.query.filter_by(name=sub).first()
+                            if not sub:
+                                flash('表格填写有错误')
+                                return redirect_back('/')
+                            student.subjects.append(sub)
+                else:
+                    flash("学生 {} 已经存在".format(i[0]))
+                db.session.commit()
+            flash("上传成功")
+        except Exception as e:
+            print(e)
+            flash('上传的表格不正确')
+            return redirect_back('/')
+
+    else:
+        flash('上传失败')
+    return redirect_back('/')
 
